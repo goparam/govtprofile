7fd0a57fa08f4d476a4da1f4021549ec002b1ffc
vajapravin <vajapravin23@gmail.com>
2013-05-01 14:04:53 +0530

    search api is as per required parameters

--- app/controllers/api/v1/grs_controller.rb
+++ app/controllers/api/v1/grs_controller.rb
@@ -1,10 +1,15 @@
 class Api::V1::GrsController <  Api::V1::ApiV1Controller
 
 	def index
-		if params[:find].nil?
+		if params[:title].nil? && params[:name].nil? && params[:to].nil? && params[:from].nil? && params[:unique_code].nil?
 			@grs = Gr.order("gr_date desc").paginate(:page => params[:page], :per_page => 10)
 		else
-			@grs = Gr.joins(:department).where("departments.name like '%#{params[:find]}%' OR grs.name like '%#{params[:find]}%'").uniq.order("gr_date desc").paginate(:page => params[:page], :per_page => 10)
+			search = []
+			search << "lower(grs.name) like '%#{params[:title].downcase}%'" if params[:title]
+			search << "(grs.gr_date >= '#{Date.parse(params[:from])}' AND grs.gr_date <= '#{Date.parse(params[:to])}')" if params[:from] && params[:to]
+			search << "grs.unique_code like '%#{params[:unique_code]}%'" if params[:unique_code]
+			search << "lower(departments.name) = '#{params[:name].downcase}'" if params[:name]
+			@grs = Gr.joins(:department).where(search.join(" OR ").to_s).uniq.order("grs.gr_date desc").paginate(:page => params[:page], :per_page => 10)
 		end
 		render "api/v1/grs/index"
 	end
