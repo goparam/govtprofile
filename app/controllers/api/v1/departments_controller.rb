6c9d6e08c09b2a4c72727ffa2e7cc72a163917a8
vajapravin <vajapravin23@gmail.com>
2013-05-08 13:19:18 +0530

    api/v1/departments api was rendering blank json -- solved

--- app/controllers/api/v1/departments_controller.rb
+++ app/controllers/api/v1/departments_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::DepartmentsController <  Api::V1::ApiV1Controller
 
 	def index
 		@depts = Department.all
+		render "api/v1/departments/index"
 	end
 end
--- app/views/api/v1/departments/index.json.rabl
+++ app/views/api/v1/departments/index.json.rabl
@@ -1,2 +1,2 @@
-collection @grs
+collection @depts
 attributes :name
