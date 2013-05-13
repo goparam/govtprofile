fa4b37c8c1ce919e751f018d32de80f1711ad030
vajapravin <vajapravin23@gmail.com>
2013-05-13 14:20:35 +0530

    URI.unescape is added to api/v1/grs controller

--- app/controllers/api/v1/grs_controller.rb
+++ app/controllers/api/v1/grs_controller.rb
@@ -5,10 +5,10 @@ class Api::V1::GrsController <  Api::V1::ApiV1Controller
 			@grs = Gr.order("gr_date desc").paginate(:page => params[:page], :per_page => 10)
 		else
 			search = []
-			search << "lower(grs.name) like '%#{params[:title].downcase}%'" if params[:title]
-			search << "(grs.gr_date >= '#{Date.parse(params[:from])}' AND grs.gr_date <= '#{Date.parse(params[:to])}')" if params[:from] && params[:to]
-			search << "grs.unique_code like '%#{params[:unique_code]}%'" if params[:unique_code]
-			search << "lower(departments.name) = '#{params[:name].downcase}'" if params[:name]
+			search << "lower(grs.name) like '%#{URI.unescape(params[:title]).downcase}%'" if params[:title]
+			search << "(grs.gr_date >= '#{Date.parse(URI.unescape(params[:from]))}' AND grs.gr_date <= '#{Date.parse(URI.unescape(params[:to])}')" if params[:from] && params[:to]
+			search << "grs.unique_code like '%#{URI.unescape(params[:unique_code])}%'" if params[:unique_code]
+			search << "lower(departments.name) = '#{URI.unescape(params[:name]).downcase}'" if params[:name]
 			@grs = Gr.joins(:department).where(search.join(" OR ").to_s).uniq.order("grs.gr_date desc").paginate(:page => params[:page], :per_page => 10)
 		end
 		render "api/v1/grs/index"
