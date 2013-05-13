6fed43fba4974419157d01a432946eefb0ec95bc
vajapravin <vajapravin23@gmail.com>
2013-05-13 15:11:04 +0530

    and date parse is removed from api/v1/grs controller

--- app/controllers/api/v1/grs_controller.rb
+++ app/controllers/api/v1/grs_controller.rb
@@ -5,10 +5,10 @@ class Api::V1::GrsController <  Api::V1::ApiV1Controller
 			@grs = Gr.order("gr_date desc").paginate(:page => params[:page], :per_page => 10)
 		else
 			search = []
-			search << "lower(grs.name) like '%#{params[:title].downcase}%'" if params[:title]
-			search << "(grs.gr_date >= '#{params[:from]}' AND grs.gr_date <= '#{params[:to]}')" if params[:from] && params[:to]
-			search << "grs.unique_code like '%#{params[:unique_code]}%'" if params[:unique_code]
-			search << "lower(departments.name) = '#{params[:name].downcase}'" if params[:name]
+			search << "lower(grs.name) like '%#{params[:title].downcase}%'" unless params[:title].nil? || params[:title].blank?
+			search << "(grs.gr_date >= '#{params[:from]}' AND grs.gr_date <= '#{params[:to]}')" unless params[:from].nil? || params[:to].nil? || params[:from].blank? || params[:to].blank?
+			search << "grs.unique_code like '%#{params[:unique_code]}%'" unless params[:unique_code].nil? || params[:unique_code].blank?
+			search << "lower(departments.name) = '#{params[:name].downcase}'" unless params[:name].nil? || params[:name].blank?
 			@grs = Gr.joins(:department).where(search.join(" OR ").to_s).uniq.order("grs.gr_date desc").paginate(:page => params[:page], :per_page => 10)
 		end
 		render "api/v1/grs/index"
