060bebfdd428170eef72007e91337fa107754002
Constantine Maring <constantine.maring@gmail.com>
2013-06-03 18:56:54 +0530

    api changed

--- app/controllers/api/v1/search_by_fields_controller.rb
+++ app/controllers/api/v1/search_by_fields_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::SearchByFieldsController <  Api::V1::ApiV1Controller
 def index
-		@members = Member.all
+		
 		
 		if params[:name].nil? && params[:email].nil?  && params[:phones].nil? && params[:diesignation].nil? && params[:current_working_location].nil?&& params[:current_workong_district].nil?
 				
 		else
 				
-				@profiles=Profile.all
+				
 				search = []
 				search << "lower(members.email) like '%#{params[:email].downcase}%'" unless params[:email].nil? || params[:email].blank?
 				search << "lower(members.phones) like '%#{params[:phones].downcase}%'" unless params[:phones].nil? || params[:phones].blank?
@@ -15,7 +15,7 @@ def index
 				search << "lower(profiles.current_workong_district) like '%#{params[:current_workong_district].downcase}%'" unless params[:current_workong_district].nil? || params[:current_workong_district].blank?
 				search << "lower(profiles.name) like '%#{params[:name].downcase}%'" unless params[:name].nil? || params[:name].blank?
 				
-				@member = Member.joins(:profiles).where(search.join(" AND ").to_s)
+				@member = Member.joins(:profiles).where(search.join(" AND ").to_s).uniq
 			
 		end
 					render "api/v1/search_by_fields/index"
