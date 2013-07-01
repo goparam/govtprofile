516107d856d0c9406222fd95e3a978e9dc353abe
Constantine Maring <constantine.maring@gmail.com>
2013-07-01 15:49:02 +0530

    error removed when ragistering app added

--- db/migrate/20130701100725_change_data_type_of_phone_from_user.rb
+++ db/migrate/20130701100725_change_data_type_of_phone_from_user.rb
@@ -1,9 +1,9 @@
 class ChangeDataTypeOfPhoneFromUser < ActiveRecord::Migration
   def self.up
-   change_column :users, :phone, :double
+   change_column :users, :phone, :string
   end
 
   def self.down
-   change_column :users, :phone, :string
+   change_column :users, :phone, :integer
   end
 end
