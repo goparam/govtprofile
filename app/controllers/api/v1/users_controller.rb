1e4a1e4b6845dc0efdee56332cf890dd71b6e6cb
Constantine Maring <constantine.maring@gmail.com>
2013-07-16 16:45:08 +0530

    change in spaling mistake in user api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,15 +13,15 @@ class Api::V1::UsersController < ApplicationController
 	def login
 		print "----------------------params = #{params}------------------------"
 
-		if params[:phone].blank? || params[:password].blank?|| params[:phone_devse].blank?
+		if params[:phone].blank? || params[:password].blank?|| params[:phone_device].blank?
 			render :json => {:success => false, :message => "Missing parameters"}, :status => 400 and return
 		end
 			user = User.find_for_database_authentication(:phone => params[:phone])
-		if ! params[:phone].eql? params[:phone_devse]
+		if ! params[:phone].eql? params[:phone_device]
 			render :json => {:success => false, :message => "Devise no and phon no are not same"}, :status => 400 and return
 		end
 		if !user
-			render :json => {:success => false, :massage => "Invalid Phone no or Password"}and return 
+			render :json => {:success => false, :message => "Invalid Phone no or Password"}and return 
 		end
 
 		if user.valid_password?(params[:password]) 
@@ -29,10 +29,10 @@ class Api::V1::UsersController < ApplicationController
 				user.reset_authentication_token! 
 				render :json => {:success => true, :auth_token => user.authentication_token} and return
 			else
-				render :json => {:success => false, :massage => "You have signed up successfully but your account has not been approved by your administrator yet "}and return
+				render :json => {:success => false, :message => "You have signed up successfully but your account has not been approved by your administrator yet "}and return
 			end
 		else 
-			render :json => {:success => false, :massage => "Invalid Phone no or Password"}and return
+			render :json => {:success => false, :message => "Invalid Phone no or Password"}and return
 		end
 	end
 	def register
@@ -71,17 +71,17 @@ class Api::V1::UsersController < ApplicationController
 
 	def resetpassword
 		print "----------------------params = #{params}------------------------"
-		if params[:phone].blank? || params[:password].blank? || params[:new_password].blank?  || params[:phone_devse].blank? 
+		if params[:phone].blank? || params[:password].blank? || params[:new_password].blank?  || params[:phone_device].blank? 
 			render :json => {:success => false, :message => "All fild mandatory "} and return
 		end
-		if ! params[:phone].eql? params[:phone_devse]
+		if ! params[:phone].eql? params[:phone_device]
 			render :json => {:success => false, :message => "Devise no and phon no are not same"}, :status => 400 and return
 		end
 		
 		user = User.find_for_database_authentication(:phone => params[:phone])
 		
 		if !user
-			render :json => {:success => false, :massage => "Invalid Phone no or Password"}and return 
+			render :json => {:success => false, :message => "Invalid Phone no or Password"}and return 
 		end
 
 		if user.valid_password?(params[:password]) 
@@ -97,7 +97,7 @@ class Api::V1::UsersController < ApplicationController
 				
 			
 		else 
-			render :json => {:success => false, :massage => "Invalid Phone no or Password"}and return
+			render :json => {:success => false, :message => "Invalid Phone no or Password"}and return
 		end
 
 	end
