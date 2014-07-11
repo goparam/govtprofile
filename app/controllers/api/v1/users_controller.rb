c399414aff63f3ee861e843fdb20588d8700a1aa
Constantine Maring <constantine.maring@gmail.com>
2014-07-11 18:17:26 +0530

    users Controller changes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -94,25 +94,25 @@ def register
 		# @user.email="#{params[:user][:phone]}@gmail.com"
 			
 			print "----------------------params = #{params}  hello--before search----------------------\n"
-		# search1 = "lower(members.phones) like '%#{params[:user][:phone]}%'" unless params[:user][:phone].nil? || params[:user][:phone].blank?
-  #       search2 = "(members.email) like '%#{params[:user][:email]}%'" unless params[:user][:email].nil? || params[:user][:email].blank?
-  #       search3 = []
-  #       search3 << "lower(profiles.name) like '%#{params[:user][:name].downcase}%'" unless params[:user][:name].nil? || params[:user][:name].blank?
-  #       search3 << "lower(profiles.name) like '%#{params[:user][:last_name].downcase}%'" unless params[:user][:last_name].nil? || params[:user][:last_name].blank?
-  #       print "---------------hellow after serch------------------\n"
-  #       @member1 = Member.joins(:profiles).where(search1).uniq
-  #       @member2 = Member.joins(:profiles).where(search2).uniq
-  #       @member3 = Member.joins(:profiles).where(search3.join(" AND ").to_s).uniq
+		search1 = "lower(members.phones) like '%#{params[:user][:phone]}%'" unless params[:user][:phone].nil? || params[:user][:phone].blank?
+        search2 = "(members.email) like '%#{params[:user][:email]}%'" unless params[:user][:email].nil? || params[:user][:email].blank?
+        search3 = []
+        search3 << "lower(profiles.name) like '%#{params[:user][:name].downcase}%'" unless params[:user][:name].nil? || params[:user][:name].blank?
+        search3 << "lower(profiles.name) like '%#{params[:user][:last_name].downcase}%'" unless params[:user][:last_name].nil? || params[:user][:last_name].blank?
+        print "---------------hellow after serch------------------\n"
+        @member1 = Member.joins(:profiles).where(search1).uniq
+        @member2 = Member.joins(:profiles).where(search2).uniq
+        @member3 = Member.joins(:profiles).where(search3.join(" AND ").to_s).uniq
         
     	print "---------------after member find------------------\n"
         
-        # if 	@member1.length > 0
-        # 	@user.member_id=@member1[0].id
-        # elsif @member2.length > 0
-        # 	@user.member_id=@member2[0].id
-        # elsif @member3.length > 0
-        # 	@user.member_id=@member3[0].id
-        # end
+        if 	@member1.length > 0
+        	@user.member_id=@member1[0].id
+        elsif @member2.length > 0
+        	@user.member_id=@member2[0].id
+        elsif @member3.length > 0
+        	@user.member_id=@member3[0].id
+        end
 		
 		if @user.save 
 			
