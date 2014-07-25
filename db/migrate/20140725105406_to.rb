4a7aceeccd59fd8ca9e29393a0b2c466e29650e4
gopal <grptjalore@gmail.com>
2014-07-25 19:14:23 +0530

    members api

--- db/migrate/20140725105406_to.rb
+++ db/migrate/20140725105406_to.rb
@@ -1,6 +1,6 @@
 class To < ActiveRecord::Migration
   def up
-  	add_column :profiles, :last_name, :string
+  	# add_column :profiles, :last_name, :string
     add_column :profiles, :father_name, :string
     add_column :profiles, :year_of_joining, :string
     add_column :profiles, :phone, :string
